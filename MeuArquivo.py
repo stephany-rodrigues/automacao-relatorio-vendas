bd2f6c1777d921af8449487366ff99f42bb3dd4f
Stephany Rodrigues <stephany84740963@gmail.com>
2026-05-18 21:59:02 -0300

    Add files via upload

---
+++ MeuArquivo.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import win32com.client as win32
+
+# importar base de dados
+tabela_vendas = pd.read_excel('Vendasp.xlsx')
+
+# visualizar base de dados
+pd.set_option('display.max_columns', None)
+print(tabela_vendas)
+
+# faturamento por loja
+faturamento= tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+# quantidade produtos vendidos cada loja
+quantidade= tabela_vendas [['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
+
+print('-'* 50)
+# ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
+print(ticket_medio)
+
+# enviar email por relatorio
+
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'stephanyospedagogicos@gmail.com'
+mail.Subject = 'Relatório de Vendas por Loja'
+mail.HTMLBody = f'''
+<p> Prezados,</p>
+
+<p> Segue o Relatório de Vendas por cada Loja. </p>
+
+<p> Faturamento: </p>
+{faturamento.to_html(formatters={'Valor Final': 'R${:,.2f}'.format})}
+<p> Quantidade Vendida: </p>
+{quantidade.to_html()}
+
+<p> Ticket Médio dos Produtos em cada Loja: </p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
+
+<p> Qualquer dúvida estou à disposição. </p>
+
+<p> Att., </p>
+<p> Stephany </p>
+
+'''
+
+mail.Send()
+
+
+
+
+
+
+
+
+
+
+
